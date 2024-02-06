6bc84f5a580f0a29a50adfb5f7ba663d885dd00f
Julia-Rawlinson <rawlinsonjulia@gmail.com>
2024-02-06 12:15:18 -0500

    added sendgrid stuff

---
+++ netlify/functions/send-email.js
@@ -0,0 +1,30 @@
+// netlify/functions/send-email.js
+const process = require('process');
+const sendgridMail = require('@sendgrid/mail');
+
+exports.handler = async (event) => {
+  sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const data = JSON.parse(event.body);
+
+  const message = {
+    to: data.to,
+    from: 'rawlinsonjulia@gmail.com',
+    subject: data.subject,
+    text: data.message,
+  };
+
+  try {
+    await sendgridMail.send(message);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Email sent successfully" }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: error.code,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
+};
